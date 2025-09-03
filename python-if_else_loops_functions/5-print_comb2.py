321b2980d880d8d5a765fafbdeae732b5660543e
sofia1098 <sofianunez000@gmail.com>
2025-09-03 14:04:23 +0000

    6

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    print(f"{i:02d}, ", end="")
+    if (i < 10):
+        print("0{}, ".format(i), end="")
+    else:
+        print("{}, ".format(i), end="")
     if (i == 99):
-        print(f"{i}")
+        print(i)
