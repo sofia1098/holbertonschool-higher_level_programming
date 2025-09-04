72729f1af4c451504dead96c876a55e80c1857cf
sofia1098 <sofianunez000@gmail.com>
2025-09-04 17:04:30 +0000

    fixed

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-suma = add(a,b)
-print(f"{a} + {b} = {suma}")
+    a = 1
+    b = 2
+    suma = add(a,b)
+    print(f"{a} + {b} = {suma}")
