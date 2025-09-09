22d7253d756b4122157368c4ba2c9d5c0f127ea5
sofia1098 <sofianunez000@gmail.com>
2025-09-09 15:46:24 +0000

    matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row[:-1]:
+            print("{:d} ".format(i), end="")
+        if row:
+        print("{:d}".format(row[-1]))
