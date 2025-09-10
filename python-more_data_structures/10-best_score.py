26b431cff7d0f9abeea82361fb7065c7e7c2749f
sofia1098 <sofianunez000@gmail.com>
2025-09-10 22:05:14 +0000

    8

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,9 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_key = 0
-    max_value = 0
-    for key, value in a_dictionary.tems():
+    max_key, max_value = list(a_dictionary.items())[0]
+    for key, value in a_dictionary.items():
         if value > max_value:
             max_value, max_key = value, key
     return max_key
