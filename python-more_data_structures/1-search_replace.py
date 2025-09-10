f590af18a9a2d234ec55a29a5dae5b87b97e2722
sofia1098 <sofianunez000@gmail.com>
2025-09-10 21:03:31 +0000

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     lista = []
     for i in my_list:
-        if i = search:
+        if i == search:
             lista.append(replace)
         else:
             lista.append(i)
