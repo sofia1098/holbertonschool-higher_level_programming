3acbea55f650645a01b6630b430aa1d90782ac20
sofia1098 <sofianunez000@gmail.com>
2025-09-12 14:15:12 +0000

    correccion

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     nums = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]) end="")
+            print("{:d}".format(my_list[i]), end="")
             nums += 1
         except (ValueError, TypeError):
             continue
