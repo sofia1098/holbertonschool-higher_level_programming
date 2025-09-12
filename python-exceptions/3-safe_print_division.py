d9414b1ef2ec5d64e3f062f142b1f47da396b031
sofia1098 <sofianunez000@gmail.com>
2025-09-12 14:23:32 +0000

    3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionerror:
+        result = None
+    finally:
+        print("{} / {}".format(result))
+    return result
