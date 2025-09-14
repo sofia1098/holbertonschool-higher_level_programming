029d91c28931a72b28fb3b282b7cea97cb9181fa
sofia1098 <sofianunez000@gmail.com>
2025-09-14 19:56:43 +0000

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,14 +3,14 @@
 This function ands rwo integres
 """
 def add_integer(a, b=98):
- """
+    """
     Adds two integers.
 
         a: The first number (int or float).
         b: The second number (int or float).
 
     Returns: Sum of a and b.
-"""
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
