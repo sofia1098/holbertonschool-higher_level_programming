75a068bea847965bd8c9e2f5705ae6f5662e56a2
sofia1098 <sofianunez000@gmail.com>
2025-09-17 00:38:42 +0000

    Class Square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,10 @@
 
 class Square:
     """Representa un cuadrado"""
+    
     def __init__(self, size):
-        if not isinstance(szie, int):
-            raise TypeError("size must be >+ 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an intenger")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
