d82ea4e95d0acc53cde8ef2b8b07665fc3b9ea3b
sofia1098 <sofianunez000@gmail.com>
2025-09-23 15:20:49 +0000

    1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a function that returns
+the list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
-    return info(obj)
+    return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,4 +4,4 @@
 
 class MyList(list):
     def print_sorted(self):
-        print(sort(MyList))
+        print(sorted(selfs))
