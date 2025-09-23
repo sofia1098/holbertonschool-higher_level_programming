6a749ef766d2698ccda71a934e4e45a6f08cf756
sofia1098 <sofianunez000@gmail.com>
2025-09-23 15:23:14 +0000

    1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,4 +6,13 @@ the list of available attributes and methods of an object
 
 
 def lookup(obj):
+       """
+    Returns the list of available attributes and methods of an object
+
+    Args:
+        obj: The object to inspect
+
+    Returns:
+        list: List of attributes and methods
+    """
     return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,4 +4,8 @@
 
 class MyList(list):
     def print_sorted(self):
+        """
+        Prints the list in ascending sorted order
+        without modifying the original list
+        """
         print(sorted(selfs))
