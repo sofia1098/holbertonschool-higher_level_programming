110b08034b10dd68d17602c97964079b8dcd002a
sofia1098 <sofianunez000@gmail.com>
2025-09-23 16:35:34 +0000

    1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -10,8 +10,8 @@ def lookup(obj):
     Returns the list of available attributes and methods of an object
     Args:
         obj: The object to inspect
-
     Returns:
         list: List of attributes and methods
     """
     return dir(obj)
+
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+clase vacia
+"""
+
+
+class BaseGeometry:
+    """
+    esta clase esta vacia
+    """
+    def area(self):
