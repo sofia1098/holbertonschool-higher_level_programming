55c7f37c3e9ba90cd0f0e51451d6c554410283d4
sofia1098 <sofianunez000@gmail.com>
2025-09-29 00:03:26 +0000

    2

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -2,15 +2,13 @@
 """ comentario"""
 
 class SwimMixin:
-
     def swim(self):
-        print("The crature swims!")
+        print("The creature swims!")
 
 class FlyMixin:
     def fly(self):
         print("The creature flies!")
 
 class Dragon(SwimMixin, FlyMixin):
-
     def roar(self):
         print("The dragon roars!")
