f9a36aeff862392b804e673dc7e33e3d2a456665
sofia1098 <sofianunez000@gmail.com>
2025-10-01 13:40:08 +0000

    ej 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     with open(filename, enconding="utf-8") as f:
-    contenido = f.read()
-    print(contenido)
+        contenido = f.read()
+        print(contenido)
