c45b80a539b9725c8c33ab0c78647a322ce884b3
sofia1098 <sofianunez000@gmail.com>
2025-10-01 13:53:37 +0000

    1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" comentario """
+
+
+def write_file(filename="", text=""):
+    with open(filename, w, encoding="utf-8") as f
+    new_file = f.write(text)
+    print(new_file)
