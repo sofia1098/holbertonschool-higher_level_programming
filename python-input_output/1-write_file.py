e71bac71ba3f752ea788c22b681b6509e7198709
sofia1098 <sofianunez000@gmail.com>
2025-10-02 13:17:33 +0000

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,5 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, w, encoding="utf-8") as f:
-    new_file = f.write(text)
-    print(new_file, end="")
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
