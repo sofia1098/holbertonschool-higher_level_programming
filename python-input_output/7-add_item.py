55c0bafd51a9b3f1f8177af7037530a5f0091dda
sofia1098 <sofianunez000@gmail.com>
2025-10-02 14:35:45 +0000

    2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
-    filename = "add_item.json" 
+    filename = "add_item.json"
+    if os.path.exists("./add_item.json"):
+        lista = list(load_from_json_file(filename))
+    else:
+        my_list = []
 
-    data.extend(sys.argv[1:])
-    save_to_json_file(data, filename)
+    lista.extend(sys.argv[1:])
+    save_to_json_file(lista, filename)
