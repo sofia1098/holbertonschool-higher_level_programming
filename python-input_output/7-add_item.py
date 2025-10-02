37b44d5d6f8fe8d42cd0cc354871065102f87aad
sofia1098 <sofianunez000@gmail.com>
2025-10-02 14:28:34 +0000

    2

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,13 +7,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
-    try:
-        data = load_from_json_file(filename)
-    except Exception:
-        data = []
-
-    #if not isinstance(data, list):
-        #data = [data]
+    filename = "add_item.json" 
 
     data.extend(sys.argv[1:])
     save_to_json_file(data, filename)
