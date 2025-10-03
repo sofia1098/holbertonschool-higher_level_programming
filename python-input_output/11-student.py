89448ce41751a78e035ae1bd76d1b91855c7ea3a
sofia1098 <sofianunez000@gmail.com>
2025-10-03 14:32:15 +0000

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module that defines Student class """
+
+
+class Student:
+    """ comentario """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            """comprension de diccionario, k se guarda temp.
+             devuelve los atri que esten en la lista y en el obj"""
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
