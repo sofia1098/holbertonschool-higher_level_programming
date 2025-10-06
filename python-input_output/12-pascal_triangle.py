9b2237ff82facb72f9a71e319df54443f8dbea08
sofia1098 <sofianunez000@gmail.com>
2025-10-06 13:06:58 +0000

    12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,14 +2,14 @@
 """ Triangulo Pascal """
 
 
-def pascal_triangle(n): 
+def pascal_triangle(n):
     """Return Pascal’s triangle of n as a list of lists"""
     if n <= 0:
         return []
 
     matriz = [[1]] 
 
-    for i in range (1, n): 
+    for i in range (1, n):
         prev = matriz[-1]
         row = [1]
 
