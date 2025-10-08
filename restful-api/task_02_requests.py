78c8ccb7c1e00279a716885a27699ef81104589f
sofia1098 <sofianunez000@gmail.com>
2025-10-08 22:25:07 +0000

    task02

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,6 @@
-import request 
-import cvs
-"""Get api usando request.
+import requests
+import csv
+"""Get api usando requests.
  1. get api y print codigo de estado
  2. checker respuesta, parsear a json, devuelve list de dict
  3.
@@ -9,13 +9,13 @@ import cvs
 
 def fetch_and_print_posts():
     
-    response = request.get(https://jsonplaceholder.typicode.com/posts)
-    
+    response = request.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
-        post = response.json()
-        data = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in posts]
+        posts = response.json()
+        data = [{"id": p["id"], "title": p["title"], "body": p["body"]} for p in posts]
 
-        with open("post.csv", "w", newline="", encoding= "utf-8") as csvf:
+        with open("posts.csv", "w", newline="", encoding= "utf-8") as csvf:
             writer = csv.DictWriter(csvf, fieldnames=["id", "title", "body"])
             writer.writeheader()
             writer.writerows(data)
