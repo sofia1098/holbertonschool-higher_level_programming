edcadb9be19f62362bad627d59a48cddfa7d28cd
sofia1098 <sofianunez000@gmail.com>
2025-10-17 23:15:29 -0300

    task05 fix

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify, request
-import secrets
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
-
 """ API REST con autenticación básica y JWT """
 
 
@@ -33,8 +31,8 @@ def login():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
-
     user = users.get(username)
+
     if not user or not check_password_hash(user["password"], password):
         return jsonify({"error": "Invalid credentials"}), 401
 
@@ -51,20 +49,20 @@ def jwt_protected():
 def admin_only():
     current_user = get_jwt_identity()
     if current_user["role"] != "admin":
-        return "error Admin access only", 403
+        return jsonify({"error": "Admin access required"}), 403
     return "Admin Access: Granted", 200
 
 @jwt.unauthorized_loader
-def unauthorized_response(callback):
-    return "Missing Authorization Header", 401
+def handle_unauthorized(err):
+    return jsonify({"error": "Missing Authorization Header"}), 401
 
 @jwt.invalid_token_loader
-def invalid_token_response(callback):
-    return "Invalid token", 401
+def handle_invalid(err):
+    return jsonify({"error": "Invalid token"}), 401
 
 @jwt.expired_token_loader
-def expired_token_response(jwt_header, jwt_payload):
-    return "Token has expired", 401
+def handle_expired(jwt_header, jwt_payload):
+    return jsonify({"error": "Token has expired"}), 401
 
 if __name__ == '__main__':
-     app.run(debug=True)
+    app.run(debug=True)
