e935888cfb173697a805dbff5ec9d9bbaa3c92c4
sofia1098 <sofianunez000@gmail.com>
2025-10-27 15:47:04 +0000

    14, 15, 16

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- comentario
+
+SELECT AVG(score) AS average FROM second_table;
---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- comentario
+
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
