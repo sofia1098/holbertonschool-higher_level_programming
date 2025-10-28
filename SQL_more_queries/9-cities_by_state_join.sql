ba365db122a79083002f29204f967314136c04c6
sofia1098 <sofianunez000@gmail.com>
2025-10-28 16:26:12 +0000

    Add: Task 09, 10

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,6 @@
--- Subquery
+-- List all cities with state name(JOIN)
 
-SELECT id, name
+SELECT cities.id, cities.name, states.name
 FROM cities
-where state_id = (
-    select id
-    from states
-    where name = 'California'
-)
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC
