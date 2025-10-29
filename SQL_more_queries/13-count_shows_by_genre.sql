526d9552d7e9d3c12c1a871fc2ca0c88be64972b
sofia1098 <sofianunez000@gmail.com>
2025-10-29 16:13:05 +0000

    Fix: task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- JOIN, count y group by
 
-SELECT tv_genre.name AS genre, COUNT(tv_show_genre.show_id) AS number_of_shows
-FROM tv_genres 
-JOIN tv_show_genre ON tv_show_genre.id = tv_show_genre.genre_id
-GROUP BY tv_show_genres.name
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
