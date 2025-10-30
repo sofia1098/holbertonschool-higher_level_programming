c4fe6ec18ab66c91f69113693327b2827511ce9b
sofia1098 <sofianunez000@gmail.com>
2025-10-30 15:08:49 +0000

    fix: me olvido de las comas

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username
+        user=username,
         passwd=password,
         db=database,
     )
