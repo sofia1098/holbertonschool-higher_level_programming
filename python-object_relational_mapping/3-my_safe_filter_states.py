6d401461393de61c733d8dc64ec76a2e4524608c
sofia1098 <sofianunez000@gmail.com>
2025-10-31 16:00:58 +0000

    Add commas

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv.[3]
+    database = sys.argv[3]
     state_name = sys.argv[4]
     
     db = MySQLdb.connect(
