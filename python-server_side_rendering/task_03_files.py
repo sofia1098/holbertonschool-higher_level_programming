f517acee58bcc57c73621964482e0ddddb8bd79f
sofia1098 <sofianunez000@gmail.com>
2025-11-19 17:28:39 +0000

    3

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+import csv
+import json
+
+app = Flask(__name__)
+
+def load_json:
+
+
+def load_
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
